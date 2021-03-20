5ca06329678a83f0a1b1789ca9cd32665c3cd467
taldan147 <tald96@gmailc.om>
2021-03-20 10:37:22 +0200

    beggging of part 3 complete

--- src/part3/find.ts
+++ src/part3/find.ts
@@ -8,7 +8,9 @@ const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
     throw "No element found.";
 }
 
-export const findResult = undefined;
+export const findResult = <T>(pred: (x: T) => boolean, a: T[]): Result<T> => {
+    return a.filter(pred).length ===0 ? makeFailure('No element found') : makeOk(a.filter(pred)[0]);
+};
 
 /* Client code */
 const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
@@ -20,6 +22,10 @@ const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
     }
 }
 
-export const returnSquaredIfFoundEven_v2 = undefined;
+export const returnSquaredIfFoundEven_v2 = (arr:number[]) : Result<number> =>{
+    return bind(findResult((a:number)=>a%2==0, arr), (num)=>makeOk(num*num));
+};
 
-export const returnSquaredIfFoundEven_v3 = undefined;
+export const returnSquaredIfFoundEven_v3 = (arr : number[]) : number=>{
+    return either(findResult((a:number)=>a%2==0,arr), ((num)=>(num*num)),((message:string)=>-1));
+};
