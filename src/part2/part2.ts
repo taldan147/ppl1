7d80babd3eff58a83bd6c9399737735ec16d6ed0
idocarmi <idocarmi2205@gmail.com>
2021-03-24 16:28:47 +0200

    made code better

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -5,22 +5,15 @@ import { splitWhen } from "ramda";
 
 const stringToArray = R.split("");
 
+//for question 1
+
 const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
 
-const notEqual= (value:string) => R.compose(R.not,R.equals(value));
- 
-const isVowel = (char : string) : boolean =>{
-    return vowels.filter((a)=>a===char).length === 0 ? false : true;
-};
+const countVowelsPipe = R.pipe(
+    R.map((a: string) => vowels.includes(a) ? 1 : 0),
+    R.reduce((acc: number, curr: number) => acc + curr, 0));
 
-
-
-
-
-/* Question 1 */
-export const countVowels = (str : string):number =>{
-    return stringToArray(str).map((a : string) => isVowel(a) ? 1 : 0).reduce((acc : number,curr : number)=> acc + curr, 0);
-} ;
+//for question 2
 
 const mapToCounts = (orig : string[], counts : number[]) : number[] =>{
     if (orig.length===0)
@@ -29,14 +22,7 @@ const mapToCounts = (orig : string[], counts : number[]) : number[] =>{
     return mapToCounts(R.remove(0, newCounts[newCounts.length-1], orig), newCounts);
 };
 
-/* Question 2 */
-export const runLengthEncoding = (str : string):string =>{
-    const strArr = stringToArray(str);
-    const uniqStrings = R.dropRepeats(strArr);
-    const g = R.join('',R.unnest(R.zip(uniqStrings, mapToCounts(strArr, []))).filter((a : string | number)=> typeof(a)!=='number' || a!==1))
-    return g;
-};
-
+//for question 3
 
 const parentheses = ['{', '}', '(', ')', '[', ']'];
 
@@ -60,8 +46,25 @@ const isPairedRecursion = (parArr : string[], myStack : string[], ind : number) 
     return myStack.length===0 ? false : isPair(myStack[myStack.length-1],parArr[ind]) && isPairedRecursion(parArr, R.remove(myStack.length-1,1, myStack), ind+1);
 }
 
+
+/* Question 1 */
+export const countVowels = (str: string): number => {
+    return countVowelsPipe(stringToArray(str));
+};
+
+/* Question 2 */
+export const runLengthEncoding = (str: string): string => {
+    //drops the uniques and counts how many of each letterare in a row (maptoCounts)
+    //then zips the arrays together
+    //then unnests them and then filters and joins
+    //the filter makes sure if there was only one letter repeating to not include it's count
+    //and finally returns
+    return R.join('',R.unnest(R.zip(R.dropRepeats(stringToArray(str)),mapToCounts(stringToArray(str), [])))
+        .filter((a: string | number) => typeof (a) !== 'number' || a !== 1));
+
+};
+
 /* Question 3 */
 export const isPaired = (str : string) : boolean => {
-    const parArr = stringToArray(str).filter((a : string)=> isParentheses(a));
-    return isPairedRecursion(parArr, [], 0);
+    return isPairedRecursion(stringToArray(str).filter((a : string)=> isParentheses(a)), [], 0);
 };
